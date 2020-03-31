f334ea0efa27937cd686420da291fa282696e93e
wangyusong-star <1062785575@qq.com>
2020-03-31 22:07:25 +0800

    03/31-4

--- OnlineShopping/WebContent/login.jsp
+++ OnlineShopping/WebContent/login.jsp
@@ -4,13 +4,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>注册界面</title>
+<title>登陆界面</title>
 </head>
 <body>
-<form action="/OnlineShopping/ARegist" method="post">
+<form action="/OnlineShopping/ALogin" method="post">
 		<table border="1" align="center" width="0.2">
 			<tr
-				style="background-color: lightgreen; color: white; font-size = 20px; font-weight: bold; text-align: center">
+				style="background-color: lightred; color: white; font-size = 20px; font-weight: bold; text-align: center">
 				<td colspan="2"><b>UserRegist</b></td>
 				
 				</tr>
@@ -23,12 +23,7 @@
 				<td>Password</td>
 				<td><input type="password" name="again_pwd" id=""></td>
 				</tr>
-				
-				<tr align="center">
-				<td>ConfirmPassword</td>
-				<td><input type="password" name="pwd" id=""></td>
-				</tr>
-				
+
 				<tr align="center">
 				<td colspan="2"><input type="submit" value="Regist"></td>
 				</tr>
---
+++ OnlineShopping/src/demo/LoginServlet.java
@@ -0,0 +1,48 @@
+package demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.XMLOperateUser;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = resp.getWriter();
+		String name;
+		String pwd;
+
+		name = req.getParameter("username");
+		pwd = req.getParameter("pwd");
+/*	
+		if() {
+			pw.print("<html>"
+			+"<head>"+"<title>注册失败</title>"+"</head>"
+			+"<body>"
+			+"<h1>"+"<a herf=\"javascript:;\" onClick=\"javascript:history.back(-1);\">两次输入的密码不一致!点击返回注册界面</a>"+"</h1>"
+			+"</body>"
+			+"</html>");
+		}else {
+			try {
+				XMLOperateUser.AddClinetLoginUser(name, pwd);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+			pw.print("注册成功");
+			pw.flush();
+		}
+
+}
