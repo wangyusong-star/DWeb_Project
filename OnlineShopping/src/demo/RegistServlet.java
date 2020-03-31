65988d4b11fff4f4fe87ac661535aae6f235619b
wangyusong-star <1062785575@qq.com>
2020-03-31 21:31:57 +0800

    03/31-3

--- OnlineShopping/src/demo/RegistServlet.java
+++ OnlineShopping/src/demo/RegistServlet.java
@@ -24,8 +24,12 @@ public class RegistServlet extends HttpServlet {
 		againpwd = req.getParameter("again_pwd");
 		
 		if(pwd!=againpwd) {
-			pw.print("<html>");pw.print("<head>");pw.print("<title>");pw.print("</head>");pw.print("<body>");pw.print("<h1>");pw.print("<a = herf>");
-			pw.print("两次输入的密码不一致");
+			pw.print("<html>"
+			+"<head>"+"<title>注册失败</title>"+"</head>"
+			+"<body>"
+			+"<h1>"+"<a herf=\"javascript:;\" onClick=\"javascript:history.back(-1);\">两次输入的密码不一致!点击返回注册界面</a>"+"</h1>"
+			+"</body>"
+			+"</html>");
 		}else {
 			try {
 				XMLOperateUser.AddClinetLoginUser(name, pwd);
